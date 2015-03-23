a526ec4eded3b3f87d492dd7566640f4d713c602
Van Phan <VanPhan514@gmail.com>
2015-03-23 15:55:05 -0400

    getting ready to pair work

--- js/application.js
+++ js/application.js
@@ -1,61 +1,84 @@
 $(document).ready(function() {
 
-var syms = document.querySelectorAll('#kemoti span');
-// try doing the above query on the dom using jquery
-// look something like  var syms =  $('#kemoti span')
-// use console.log to debug in chrome console.
-var screenForm = [];
+//Made "var syms = document.querySelectorAll('#kemoti span');" into jquery
+var $syms = $('#kemoti span');
 
-for(var i = 0; i < syms.length; i++) {
-	syms[i].onclick = function(e) {
+var screenForm = { 
+	LARM: 'ლ',
+	LFACE: '(',
+	LEYE: 'ಠ',
+	MOUTH: '_',
+	REYE: 'ಠ',
+	RFACE: ')',
+	RARM: 'ლ'
+};
 
-		var input = document.querySelector('.screen');
-		// you named two variables "input" overwriting the previous one
-		// what did you wan to do here?
-		var input = document.getElementById("kemoti");
+$syms.each(function() {
+	var $sym = $(this);
+
+	$sym.on('click', function(event) {
+		var $clickedOn = $(event.target);
+		// console.log($clickedOn.parent().hasClass('faces'));
+		if (($clickedOn.parent().hasClass('faces')) === true)
+			console.log('WOO');
+	});
+});
+
+console.log(screenForm.parse(screenForm.stringify(screenForm)));
 
 
-		var inputClass = input.className;
-		var inputVal = input.innerHTML;
 
 
-		// what is "this"?
-		var btnVal = this.innerHTML;
+// for(var i = 0; i < syms.length; i++) {
+// 	syms[i].onclick = function(e) {
+
+// 		var input = document.querySelector('.screen');
+// 		// you named two variables "input" overwriting the previous one
+// 		// what did you wan to do here?
+// 		var input = document.getElementById("kemoti");
 
 
-		if(btnVal == 'C') {
-			input.innerHTML = '';
-		}
+// 		var inputClass = input.className;
+// 		var inputVal = input.innerHTML;
 
-		switch(inputClass){
-		    case "faces":
-			    input.innerHTML = "I have class1";
-			    break;
-		    case "top":
-			    input.innerHTML = "I have class2";
-			    break;
-		    case "class3":
-			    input.innerHTML = "I have class3";
-			    break;
-		    case "class4":
-			    input.innerHTML = "I have class4";
-			    break;
-		    default:
-			    input.innerHTML = "Hi";
-			    break
-		}
 
-		// if else(btnVal.contains(faces)) {
-		// 	input.innerHTML += btnVal;
-		// }
+// 		// what is "this"?
+// 		var btnVal = this.innerHTML;
 
-		// else {
-		// 	input.innerHTML += btnVal;
-		// }
 
-		// prevent page jumps
-		e.preventDefault();
-	}
-}
+// 		if(btnVal == 'C') {
+// 			input.innerHTML = '';
+// 		}
+
+// 		switch(inputClass){
+// 		    case "faces":
+// 			    input.innerHTML = "I have class1";
+// 			    break;
+// 		    case "top":
+// 			    input.innerHTML = "I have class2";
+// 			    break;
+// 		    case "class3":
+// 			    input.innerHTML = "I have class3";
+// 			    break;
+// 		    case "class4":
+// 			    input.innerHTML = "I have class4";
+// 			    break;
+// 		    default:
+// 			    input.innerHTML = "Hi";
+// 			    break
+// 		}
+
+// 		// if else(btnVal.contains(faces)) {
+// 		// 	input.innerHTML += btnVal;
+// 		// }
+
+// 		// else {
+// 		// 	input.innerHTML += btnVal;
+// 		// }
+
+// 		// prevent page jumps
+// 		e.preventDefault();
+// 	}
+// }
  });
 

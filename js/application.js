b5a0d71e0f5c70fa1370690931c3013c8f9cf8b0
Van Phan <VanPhan514@gmail.com>
2015-05-08 11:48:06 -0400

    Adding a button to hide span elements(Work in progress)

--- js/application.js
+++ js/application.js
@@ -6,27 +6,32 @@ var $syms = $('#kemoti span');
 		$sym.on('click', function(event) {
 			var $clickedOn = $(event.target);
 			var input = $clickedOn.text();
-			console.log(input);
 			var classname = "." + $clickedOn.parent()[0].getAttribute("class") + "Slot";
-			console.log(classname);
 			if (classname == ".faceSlot") {
 				var arr = input.split("  ");
 				$(".lfaceSlot").text(arr[0]);
 				$(".rfaceSlot").text(arr[1]);
+				//I returned null to stop the function here
 				return null;
 			}
-			var print = $(classname).text(input);
+			$(classname).text(input);
 		});
 	});
 
 });
 
+function toggle_visibility(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+    }
+
 function copyToClipboard() {
 	var text = larmSlot.innerHTML + lfaceSlot.innerHTML + leyeSlot.innerHTML 
-		+ mouthSlot.innerHTML + reyeSlot.innerHTML + rfaceSlot.innerHTML + rarmSlot.innerHTML;
+		+ mouthSlot.innerHTML + reyeSlot.innerHTML + rfaceSlot.innerHTML 
+		+ rarmSlot.innerHTML + itemSlot.innerHTML;
 	window.prompt("Copy to clipboard: Ctrl+C, Enter", text);
 }
-
-
-
 
